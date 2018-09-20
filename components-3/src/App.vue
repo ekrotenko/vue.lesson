4b7d2e5634983497f2cd75f9a4b9a376dce53a8b
eugene.k <ekrotenko.ua@gmail.com>
2018-09-20 09:25:14 +0300

    passed components #3 lesson

---
+++ components-3/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <my-header></my-header>
+    <div class="container">
+      <products></products>
+      <product-details></product-details>
+    </div>
+  </div>
+</template>
+
+<script>
+  import myHeader from './Header.vue';
+  import ProductList from './ProductsList.vue';
+  import ProductDetails from './ProductDetails.vue';
+
+  export default {
+    data() {
+      return {
+      }
+    },
+    components: {
+      myHeader,
+      'products': ProductList,
+      ProductDetails
+    },
+  }
+</script>
+
+<style>
+ .container{
+   display:flex;
+   align-items: flex-start;
+ }
+</style>
